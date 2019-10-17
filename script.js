38e82e92d61515ea178ffcbce18df07f979aaaf4
Zbeulitox <paulosimon.machado@viacesi.fr>
2019-10-17 17:18:36 +0200

    ajout bouton supprimer + design

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-// Create a new list item when clicking on the "Add" button
+// Créer un nouvel élément lorsque qu'on clique sur le bouton "Ajouter"
 function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') { //Si il n'y a aucun texte
-      alert("Champ invalide");
-    } else {
-      document.getElementById("myUL").appendChild(li);
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+//Suppresion de la ligne lorsque l'on clique sur le bouton
+  var close = document.getElementsByClassName("close");
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
     }
-    document.getElementById("myInput").value = "";
-                      } 
+  }
+}
+// Permet de créer un bouton
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
